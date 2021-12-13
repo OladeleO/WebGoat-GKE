e25a1e40d8a372057648a5a4fa5df185bda5fa97
OladeleO <58373625+OladeleO@users.noreply.github.com>
2021-12-13 19:14:50 +0000

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,15 @@
+provider "google" {
+  version = "3.53"
+  project = var.project
+  region  = var.region
+  zone    = var.region
+
+}
+
+
+resource "google_container_cluster" "my_vpc_native_cluster" {
+  name               = var.gke_cluster_name
+  location           = var.zone
+  initial_node_count = 3
+
+}
